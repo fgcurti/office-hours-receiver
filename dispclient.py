f776be15ab46e4079c42dd5fb80fb929c1068018
Michael Day <day.michael.b@gmail.com>
2019-11-15 19:37:52 -0600

    oops

--- dispclient.py
+++ dispclient.py
@@ -3,6 +3,12 @@ import time
 import requestfetcher
 import os
 
+def current_milli():
+    return int(round(time.time() * 1000))
+
+def get_pressed():
+    return False
+
 if __name__ == "__main__":
     rf = requestfetcher.RequestFetcher()
 
@@ -17,11 +23,12 @@ if __name__ == "__main__":
     while True:
         if current_milli() >= lastchecked_ms + 1000:
             req_str = rf.get_req()
-            ### debug
+            # debug
             print("tick")
             lastchecked_ms = current_milli()
         if req_str != "":
             if process_handle == None:
+                # placeholder
                 process_handle = subprocess.Popen(["ls", "-a"])
         else:
             if process_handle != None:
@@ -29,11 +36,5 @@ if __name__ == "__main__":
                 process_handle = None
         if get_pressed() == True:
             rf.clear_req()
-
-def current_milli():
-    return int(round(time.time() * 1000))
-
-def get_pressed():
-    return False
 
 
