5b00e19b5696d544d9b17c5606a04095b716d047
Michael Day <day.michael.b@gmail.com>
2019-11-15 19:20:24 -0600

    wireframed main loop

--- dispclient.py
+++ dispclient.py
@@ -3,39 +3,37 @@ import time
 import requestfetcher
 import os
 
-rf = requestfetcher.RequestFetcher()
+if __name__ == "__main__":
+    rf = requestfetcher.RequestFetcher()
 
-#####################
-# TEMP WORKING STUFF#
+    print(rf.get_req())
 
-print(rf.get_req())
+    lastchecked_ms = 0
+    req_str = ""
+    process_handle = None
 
-os.system("echo -e \"\\a\"")
+    print(current_milli)
+
+    while True:
+        if current_milli() >= lastchecked_ms + 1000:
+            req_str = rf.get_req()
+            ### debug
+            print("tick")
+            lastchecked_ms = current_milli()
+        if req_str != "":
+            if process_handle == None:
+                process_handle = subprocess.Popen(["ls", "-a"])
+        else:
+            if process_handle != None:
+                process_handle.kill()
+                process_handle = None
+        if get_pressed() == True:
+            rf.clear_req()
+
+def current_milli():
+    return int(round(time.time() * 1000))
 
 def get_pressed():
     return False
-
-####################
-
-lastchecked_ms = 0
-req_str = ""
-process_handle = None
-current_milli = lambda: int(round(time.time() * 1000))
-
-while True:
-    if current_milli >= lastchecked_ms + 1000:
-        req_str = rf.get_req()
-        ### debug
-        print("tick")
-        lastchecked_ms = current_milli
-    if req_str != "":
-        if process_handle == None:
-            process_handle = subprocess.Popen(["ls", "-a"])
-    else:
-        if process_handle != None:
-            process_handle.kill()
-            process_handle = None
-    if get_pressed() == True:
-        rf.clear_req()
 
 
