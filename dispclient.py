a38160a99793e777b7f273eb73eb86deef993f58
fgcurti <46764922+fgcurti@users.noreply.github.com>
2020-03-01 16:05:55 -0600

    Modify Select statement and add LED code

--- dispclient.py
+++ dispclient.py
@@ -1,7 +1,6 @@
 import subprocess
 import time
 import requestfetcher
-import os
 
 def current_milli():
     return int(round(time.time() * 1000))
@@ -9,32 +8,39 @@ def current_milli():
 def get_pressed():
     return False
 
+def runled(name_list):
+    process_handle = None
+    for i in range(0,len(name_list)):
+        process_handle = subprocess.Popen(["sudo",\
+        "../display16x32/rpi-rgb-led-matrix/examples-api-use/scrolling-text-example",\
+        "-f","../display16x32/rpi-rgb-led-matrix/fonts/6x10.bdf",\
+        "--led-no-hardware-pulse", "-l", "1", "--led-rows=16",\
+        "--led-cols=32", "-s", "2",name_list[i]])
+        subprocess.check_call(["sudo", "kill",str(process_handle.pid)])
+        time.sleep(7)
+
 if __name__ == "__main__":
     rf = requestfetcher.RequestFetcher()
 
-    print(rf.get_req())
-
     lastchecked_ms = 0
-    req_str = ""
     process_handle = None
-
+    req_list_old = [-999]
+    
     print(current_milli)
 
     while True:
         if current_milli() >= lastchecked_ms + 1000:
-            req_str = rf.get_req()
-            # debug
+            req_list = rf.get_req()
             print("tick")
             lastchecked_ms = current_milli()
-        if req_str != "":
-            if process_handle == None:
-                # placeholder
-                process_handle = subprocess.Popen(["ls", "-a"])
-        else:
-            if process_handle != None:
-                process_handle.kill()
-                process_handle = None
-        if get_pressed() == True:
-            rf.clear_req()
-
+            if req_list != [] and req_list_old != req_list:
+                req_list_old = req_list[:]
+                runled(req_list)
+            elif req_list == [] and req_list_old != req_list:
+                req_list_old = req_list[:]
+            elif req_list != [] and req_list_old == req_list:
+                runled(req_list)
+            if get_pressed() == True:
+                #rf.clear_req()
+                pass
 
--- requestfetcher.py
+++ requestfetcher.py
@@ -9,14 +9,20 @@ class RequestFetcher:
         passwd=cfg.mysql['passwd'],
         database=cfg.mysql['database']
     )
-
+    db.start_transaction(isolation_level='READ COMMITTED')
+    
     def get_req(self):
         curs = self.db.cursor()
-        curs.execute("SELECT u.name "
-                    "  FROM users u "
-                    "  JOIN requests r "
-                    "    ON u.userid = r.userid")
-        return curs.fetchone()
+        curs.execute("SELECT u.name FROM users u "\
+                     "JOIN requests r ON u.userid = r.userid "\
+                     "WHERE current_timestamp < date_add(date, INTERVAL 2 MINUTE);")
+        ret_list = []
+        records = curs.fetchall()
+        for row in records:
+            ret_list.append(row[0])
+
+        curs.close()
+        return ret_list
 
     def clear_req(self):
         curs = self.db.cursor()
